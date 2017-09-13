0ea08a67635f1865fef0d1d790d503cb5d99c7f7
gabbley <baniquedg@bishopireton.org>
2017-09-13 14:59:36 -0400

    fix logic

--- LargeInteger/src/LargeInteger.java
+++ LargeInteger/src/LargeInteger.java
@@ -1,12 +1,35 @@
 
-public class LargeInteger {
+import java.util.Arrays;
 
-	private static int add(int[] a, int[] b) {
-		int sum = 0;
-		for (int i = 0; i<a.length; i++) {
-			sum += a[i] + b[i];
-		}
-		return sum;
+public class LargeInteger {
+	
+	public static void main(String[] args) {
+		
+		int[] test1 =  new int[] {1,5,3,9};
+		int[] test2 = new int[] {0, 4, 5, 2};
+		
+		System.out.println(Arrays.toString(add(test1, test2)));
 	}
+
+	public static int[] add(int[] a, int[] b) {
+		int sum = 0;
+		int[] total = new int[a.length];
+		
+		
+		for (int i = a.length-1; i<0; i++) {
+			sum = a[i] + b[i];
+			if(sum>10) {
+				total[a.length-i] = 0;
+				a[a.length-i-1] += 1;
+			}
+			else {
+				total[i] = sum;
+			}
+		
+		}
+		return total;
+	}
+	
+
 }
 
