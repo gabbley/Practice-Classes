8ceee157be21cf560d4156ecaed34b405fa6f335
gabbley <baniquedg@bishopireton.org>
2017-10-22 21:57:01 -0400

    Idk i havent tested it but i just followed the thing

--- IndexMaker/src/DocumentIndex.java
+++ IndexMaker/src/DocumentIndex.java
@@ -1,4 +1,47 @@
+import java.util.ArrayList;
 
-public class DocumentIndex {
+public class DocumentIndex extends ArrayList<IndexEntry> {
+
+	public DocumentIndex() {
+		super();
+	}
+
+	public DocumentIndex(int size) {
+		super(size);
+	}
+
+	public void addWord(String word, int num) {
+		int insert = foundOrInserted(word);
+		this.get(insert).add(num);
+	}
+
+	public void addAllWords(String str, int num) {
+		String[] strArr = str.split("\\W+");
+		for (String word : strArr){
+			if(!word.equals(""))
+			addWord(word, num);
+		}
+	}
+
+	private int foundOrInserted(String word) {
+		for (int i = 0; i < this.size(); i++) {
+			String findWord = this.get(i).getWord();
+
+			// if word to find already has index entry
+			if (findWord.equalsIgnoreCase(word)) {
+				return i;
+			}
+
+			// if index entry doesn't exist, inserts index entry alphabetically
+			else if (findWord.compareToIgnoreCase(word) > 0) {
+				this.add(i, new IndexEntry(word));
+			}
+
+		}
+
+		// otherwise just add to the end
+		this.add(new IndexEntry(word));
+		return this.size();
+	}
 
 }
--- IndexMaker/src/IndexMaker.java
+++ IndexMaker/src/IndexMaker.java
@@ -70,4 +70,6 @@ public class IndexMaker
     keyboard.close();
     System.out.println("Done.");
   }
+  
+  
 }
